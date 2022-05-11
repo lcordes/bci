42e1955c1963fa465269c7b9aff364c81eed6ede
Lars Cordes <l.cordes@student.rug.nl>
2022-05-11 17:54:30 +0200

    refactored Experiment GUI

---
+++ src/apps/experiment_gui.py
@@ -0,0 +1,105 @@
+import pygame
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TRIALS_PER_CLASS = int(os.environ["TRIALS_PER_CLASS"])
+TRIAL_END_MARKER = int(os.environ["TRIAL_END_MARKER"])
+IMAGERY_PERIOD = int(float(os.environ["IMAGERY_PERIOD"]) * 1000)
+CLASSES = os.environ["CLASSES"].split(",")
+X_SIZE = int(os.environ["X_SIZE"])
+Y_SIZE = int(os.environ["Y_SIZE"])
+
+FRONT_COL = (255, 255, 255)  # white
+BACK_COL = (0, 0, 0)  # black
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+SHAPE_RADIUS = 15
+PYGAME_KEYS = {"space": pygame.K_SPACE, "esc": pygame.K_ESCAPE}
+
+
+class ExperimentGUI:
+    def __init__(self, keep_log):
+        pygame.init()
+        self.keep_log = keep_log
+        self.window = pygame.display.set_mode((X_SIZE, Y_SIZE))
+        self.center = (X_SIZE // 2, Y_SIZE // 2)
+        self.state = "start"
+        self.font = pygame.font.Font("freesansbold.ttf", 26)
+        self.trial = 1
+        self.log = []
+        self.running = True
+        self.pause = False
+
+        self.window.fill(BACK_COL)
+        pygame.display.update()
+
+    def display_text(self, string, colour, loc=None, redraw=True):
+        loc = self.center if not loc else loc
+        text = self.font.render(string, True, colour)
+        textRect = text.get_rect()
+        textRect.center = loc
+        if redraw:
+            self.window.fill(BACK_COL)
+        self.window.blit(text, textRect)
+        pygame.display.update()
+
+    def display_feedback(self, probs):
+        # Determine feedback color per marker
+        cols = [FRONT_COL, FRONT_COL, FRONT_COL]
+        cols[CLASSES.index(self.current_class)] = GREEN
+        if not self.current_class == self.command:
+            cols[CLASSES.index(self.command)] = RED
+
+        # Get location per marker
+        offset = Y_SIZE // 10
+        locs = [
+            (self.center[0] - offset, self.center[1]),
+            (self.center[0] + offset, self.center[1]),
+            (self.center[0], self.center[1] + offset),
+        ]
+
+        for prob, col, loc in zip(probs, cols, locs):
+            self.display_text(str(prob), col, loc, redraw=False)
+
+    def draw_arrow(self):
+        cx = self.center[0]  # center x-a-xis
+        cy = self.center[1]  # center x-a-xis
+        l = SHAPE_RADIUS
+        s = l * 5 // 6
+        if self.current_class == "down":
+            points = [(cx, cy + l), (cx - s, cy - l), (cx + s, cy - l)]
+        elif self.current_class == "left":
+            points = [(cx - l, cy), (cx + l, cy - s), (cx + l, cy + s)]
+        elif self.current_class == "right":
+            points = [(cx + l, cy), (cx - l, cy - s), (cx - l, cy + s)]
+
+        self.window.fill(BACK_COL)
+        pygame.draw.polygon(self.window, FRONT_COL, points)
+        pygame.display.update()
+
+    def draw_circle(self):
+        self.window.fill(BACK_COL)
+        pygame.draw.circle(
+            self.window,
+            FRONT_COL,
+            center=self.center,
+            radius=SHAPE_RADIUS // 2,
+        )
+        pygame.display.update()
+
+    def key_pressed(self, key):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == PYGAME_KEYS[key]:
+                    return True
+        return False
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.exit()
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == PYGAME_KEYS["esc"]:
+                    self.pause = True
---
+++ src/apps/training_generator.py
@@ -0,0 +1,115 @@
+from experiment_gui import *
+from datetime import datetime
+import argparse
+import pygame
+import pandas as pd
+from random import shuffle
+import sys
+from pathlib import Path
+
+parent_dir = str(Path(__file__).parents[1].resolve())
+sys.path.append(parent_dir)
+from data_acquisition.data_handler import OpenBCIHandler
+
+
+class DataGenerator(ExperimentGUI):
+    def __init__(self, keep_log, board_type):
+        super().__init__(keep_log)
+        pygame.display.set_caption("Evaluator")
+        self.data_handler = OpenBCIHandler(board_type=board_type)
+        if self.data_handler.status == "no_connection":
+            print(
+                "\n",
+                "No headset connection, use '--train synthetic' for simulated data",
+            )
+            self.running = False
+        # Create shuffled list containing n TRIALS_PER_CLASS examples of all classes
+        self.trial_classes = CLASSES * TRIALS_PER_CLASS
+        shuffle(self.trial_classes)
+
+    def exit(self):
+        if self.log:
+            self.data_handler.merge_trials_and_exit()
+            if self.keep_log:
+                data = pd.DataFrame.from_dict(self.log)
+                session_time = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+                data.to_csv(f"data/recordings/Training_trials_{session_time}.csv")
+
+    def run(self):
+        while self.running:
+            if self.pause:
+                self.display_text("Press spacebar to resume", FRONT_COL)
+                while not self.key_pressed("space"):
+                    pass
+                self.pause = False
+                self.state = "arrow"
+
+            elif self.state == "start":
+                self.display_text("Start session with spacebar", FRONT_COL)
+
+                while not self.key_pressed("space"):
+                    pass
+                self.state = "arrow"
+
+            elif self.state == "fixdot":
+                self.draw_circle()
+                self.state = "imagine"
+                pygame.time.delay(1500)
+
+            elif self.state == "arrow":
+                self.current_class = self.trial_classes[self.trial - 1]
+                self.draw_arrow()
+                self.state = "fixdot"
+                pygame.time.delay(2000)
+
+            elif self.state == "imagine":
+                self.data_handler.insert_marker(CLASSES.index(self.current_class) + 1)
+                self.display_text("", FRONT_COL)
+                self.state = "trial_end"
+                pygame.time.delay(IMAGERY_PERIOD)
+                self.data_handler.insert_marker(TRIAL_END_MARKER)
+
+            elif self.state == "trial_end":
+                self.draw_circle()
+                self.state = "arrow"
+                data = {
+                    "trial": self.trial,
+                    "instruction": self.current_class,
+                }
+                print(data)
+                self.log.append(data)
+                self.trial += 1
+                self.data_handler.save_trial()
+
+                if self.trial > len(self.trial_classes):
+                    self.exit()
+                    self.running = False
+                else:
+                    pygame.time.delay(1000)
+
+            self.check_events()
+
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log",
+        help="write trial data to csv",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--board",
+        dest="board_type",
+        choices=["synthetic", "cython", "daisy"],
+        const="daisy",
+        nargs="?",
+        type=str,
+        help="Use synthetic or cython board instead of daisy.",
+    )
+    args = parser.parse_args()
+
+    generator = DataGenerator(keep_log=args.log, board_type=args.board_type)
+    generator.run()
