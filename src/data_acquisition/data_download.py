eec444d47363e6afcd3253fa5688928c28dbc0a2
Lars Cordes <l.cordes@student.rug.nl>
2022-07-17 11:53:37 +0200

    added initial script to download experiment data

---
+++ src/data_acquisition/data_download.py
@@ -0,0 +1,20 @@
+import requests
+import zipfile
+import io
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATA_URL = os.environ["DATA_URL"]
+DATA_PATH = os.environ["DATA_PATH"]
+
+
+def download_data(url, dir):
+    r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall(dir)
+
+
+if __name__ == "__main__":
+    dir = f"{DATA_PATH}/recordings/users"
+    download_data(DATA_URL, dir)
